881fe4c8e5084d29aff71fc3a5962d7cc6956ec4
Raziya Tabassum <108030074+Raziiy@users.noreply.github.com>
2022-11-23 15:41:38 +0530

    Add files via upload

---
+++ Lcm.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+ class Lcm
+ {
+
+  public static void main(String[] args) 
+{
+
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter your 1st Number");
+   
+ int n1 =sc.nextInt();
+  
+  
+System.out.println("Enter your 2nd Number");
+  
+  int n2 =sc.nextInt();
+  
+  int  lcm;
+
+ 
+   lcm = (n1 > n2) ? n1 : n2;
+
+    
+while(true) 
+{
+     
+ if( lcm % n1 == 0 && lcm % n2 == 0 ) 
+{
+
+        System.out.printf("The LCM of %d and %d is %d.", n1, n2, lcm);
+   
+     break;
+   
+   }
+    
+  ++lcm;
+ 
+   }
+ 
+ }
+
+}
